a4af454bd5ffdbaa21441b688eb1e47c3cf61f47
Dave Tickle <dave.tickle@socialfinance.org.uk>
2022-11-15 17:26:49 +0000

    feat: adding cookie gate and LA selection

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,6 @@
 import Dashboard from "./Dashboard";
 import LoadData from "./LoadData";
 import SetModel from "./SetModel";
+import Start from "./Start";
 
-export { Dashboard, LoadData, SetModel };
+export { Dashboard, LoadData, SetModel, Start };
