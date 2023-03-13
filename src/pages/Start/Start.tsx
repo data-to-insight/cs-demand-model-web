291b5b384e2834fd1662d7b9a9b1d01ad4e09471
Kaj Siebert <kaj@k-si.com>
2023-03-13 15:14:20 +0100

    Update cookie gate and favicon

--- src/pages/Start/Start.tsx
+++ src/pages/Start/Start.tsx
@@ -1,63 +1,114 @@
+/** @jsxImportSource @emotion/react */
+
 import React, { useState } from "react";
 import {
   Box,
-  Typography,
-  Button,
+  FormControl,
+  InputLabel,
   Select,
   MenuItem,
   SelectChangeEvent,
+  Typography,
+  Button,
+  Grid,
 } from "@mui/material";
 
+import { Block } from "@sfdl/sf-mui-components";
 import { laData } from "utils/authorityData";
 
-import { Block } from "@sfdl/sf-mui-components";
+declare global {
+  interface Window {
+    _gaq: any;
+  }
+}
 
-interface StartProps {
+interface StartPageProps {
   onClick: () => void;
 }
 
-const Start = (props: StartProps) => {
-  const [localAuthority, setLocalAuthority] = useState<string>(laData[0].la_id);
+const Start = (props: StartPageProps) => {
+  const handleButtonClick = () => {
+    try {
+      //@ts-ignore
+      gtag("event", "cin-la-select", {
+        localAuthority,
+        localAuthorityName: laData.filter((la) => {
+          return la.la_id === localAuthority;
+        })[0].la_name,
+        event_callback: () => {
+          props.onClick();
+        },
+        debug_mode: true,
+      });
+    } catch (err) {
+      console.log(err);
+      props.onClick();
+    }
+  };
 
+  const [localAuthority, setLocalAuthority] = useState<string>("");
   const renderDropdown = () => {
     return (
-      <Select
-        value={localAuthority}
-        onChange={(event: SelectChangeEvent) => {
-          setLocalAuthority(event.target.value as string);
-        }}
-      >
-        {laData.map((laItem) => {
-          return <MenuItem value={laItem.la_id}>{laItem.la_name}</MenuItem>;
-        })}
-      </Select>
+      <FormControl fullWidth>
+        <InputLabel id="la-select-label">Choose local authority</InputLabel>
+        <Select
+          value={localAuthority}
+          labelId="la-select-label"
+          label="Choose local authority"
+          onChange={(event: SelectChangeEvent) => {
+            setLocalAuthority(event.target.value as string);
+          }}
+        >
+          {laData.map((laItem) => {
+            return (
+              <MenuItem value={laItem.la_id} key={laItem.la_id}>
+                {laItem.la_name}
+              </MenuItem>
+            );
+          })}
+        </Select>
+      </FormControl>
     );
   };
 
   return (
-    <>
-      <Box>
-        <Block spacing="blockLarge">
-          <Typography>
-            This tool automatically forecasts demand for children’s services
-            placements so that commissioners can conduct sufficiency analyses,
-            secure appropriate budgets for services and demonstrate the business
-            case for a new or changed service.
-          </Typography>
-        </Block>
-        <Block spacing="blockLarge">{renderDropdown()}</Block>
-        <Block>
-          <Button
-            variant="contained"
-            onClick={() => {
-              props.onClick();
-            }}
-          >
-            Start
-          </Button>
-        </Block>
-      </Box>
-    </>
+    <Box flexGrow={1}>
+      <Block>
+        <Typography variant="body1">
+          Data to Insight is a national project led by local authorities with
+          support from the ADCS, DLUHC, DfE and Ofsted to help local authorities
+          make better use of data.
+        </Typography>
+      </Block>
+
+      <Block>
+        <Typography variant="body1">
+          This tool automatically forecasts demand for children’s services
+          placements so that commissioners can conduct sufficiency analyses,
+          secure appropriate budgets for services and demonstrate the business
+          case for a new or changed service.
+        </Typography>
+      </Block>
+
+      <Block spacing={"blockLarge"}>
+        <Grid container spacing={2}>
+          <Grid item xs={6}>
+            {renderDropdown()}
+          </Grid>
+        </Grid>
+      </Block>
+
+      <Block spacing={"blockLarge"}>
+        <Button
+          onClick={handleButtonClick}
+          variant="contained"
+          sx={{ boxShadow: 0 }}
+          disabled={localAuthority === ""}
+        >
+          Start
+        </Button>
+      </Block>
+    </Box>
   );
 };
 
